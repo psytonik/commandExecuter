c957920e1cd48df0e3666f987efe620b60988bbd
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-12-03 21:39:25 +0200

    Adding Prompt Service

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
+import {PromptService} from "./core/prompt.service";
+
 export class App {
-	run(){
+	async run() {
+		const res = await (new PromptService()).inputPrompt<number>('numerok', 'number');
+		console.log(res);
 		console.log('done');
 	}
 }
+
 const app = new App();
-app.run();
+app.run().then();
