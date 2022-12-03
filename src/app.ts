31d0ff11324eb8b3aa23ddeb142eb7abbeb09edd
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-12-04 01:59:48 +0200

    fixes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-// import {FfmpegExecute} from "./commands/ffmpeg/ffmpeg.execute";
+import {FfmpegExecute} from "./commands/ffmpeg/ffmpeg.execute";
 import {ConsoleLogger} from "./out/consoleLogger/console.logger";
 import {DirExecute} from "./commands/dir/dir.execute";
 
 export class App {
 	async run() {
+
 		await new DirExecute(ConsoleLogger.getInstance()).execute();
-		// await new FfmpegExecute(ConsoleLogger.getInstance()).execute();
+		await new FfmpegExecute(ConsoleLogger.getInstance()).execute();
 	}
 }
 
