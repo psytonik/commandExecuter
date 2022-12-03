0f5a3449bfc0f857aacc97fdc88af961d5ba70cd
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-12-03 23:50:07 +0200

    implementation if builder

---
+++ src/commands/ffmpeg/ffmpeg.builder.ts
@@ -0,0 +1,28 @@
+export class FfmpegBuilder {
+	private inputPath!: string;
+	private options: Map<string, string> = new Map();
+	constructor() {
+		this.options.set('-s.v', 'libx264')
+	}
+
+	input(inputPath: string): this{
+		this.inputPath = inputPath;
+		return this;
+	}
+	setVideoSize(width:number, height:number): this{
+		this.options.set('-s', `${width}x${height}`)
+		return this;
+	}
+	output(outputPath: string): string[] {
+		if(!this.inputPath){
+			throw new Error('Where original file ?')
+		}
+		const args: string[] = ['-i', this.inputPath]
+		this.options.forEach((value, key)=>{
+			args.push(key);
+			args.push(value);
+		})
+		args.push(outputPath);
+		return args;
+	}
+}
